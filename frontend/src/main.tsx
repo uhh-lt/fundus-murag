10d87f50224d46481a2848feb6713b2389286598
floschne <florian.schneider-1@uni-hamburg.de>
2025-03-21 13:11:21 +0000

    disabled react strict mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,36 +1,33 @@
-import CssBaseline from '@mui/material/CssBaseline';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
-import ChatPage from './pages/ChatPage';
-import StartPage from './pages/StartPage';
+import CssBaseline from "@mui/material/CssBaseline";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import ChatPage from "./pages/ChatPage";
+import StartPage from "./pages/StartPage";
 
-import { ChatProvider } from './context/ChatContext';
-import { ThemeProvider } from './context/ThemeContext';
+import { ChatProvider } from "./context/ChatContext";
+import { ThemeProvider } from "./context/ThemeContext";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: StartPage,
-  },
-  {
-    path: "/chat",
-    Component: ChatPage,
-  }
+    {
+        path: "/",
+        Component: StartPage,
+    },
+    {
+        path: "/chat",
+        Component: ChatPage,
+    },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <CssBaseline />
-
-    <ThemeProvider>
-      <CssBaseline />
-      <ChatProvider>
-        <RouterProvider router={router} />
-      </ChatProvider>
-    </ThemeProvider>
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    // <React.StrictMode>
+    <>
+        <CssBaseline />
+        <ThemeProvider>
+            <CssBaseline />
+            <ChatProvider>
+                <RouterProvider router={router} />
+            </ChatProvider>
+        </ThemeProvider>
+    </>,
+    // </React.StrictMode>
+);
